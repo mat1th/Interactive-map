faf09c84545e5329d6329dcd52ee039c6bed2416
Matth96 <Matthias.Dolstra@hva.nl>
2015-10-05 23:12:56 +0200

    Get from rawData the location of the trashes

--- server/server.js
+++ server/server.js
@@ -4,16 +4,39 @@ Meteor.publish("markers", function () {
     return Markers.find();
 });
 
-
 //data collection
 var data = new Meteor.Collection('data');
 Meteor.publish("data", function () {
     return data.find();
 });
 
+var rawData = HTTP.get("https://spreadsheets.google.com/feeds/list/1mkgRVyHAmTS6VhL8q74XKdsF_adAVdNvTTilJshO5pY/od6/public/values?alt=json").data;
 
-rawData = HTTP.get("https://spreadsheets.google.com/feeds/list/1mkgRVyHAmTS6VhL8q74XKdsF_adAVdNvTTilJshO5pY/od6/public/values?alt=json").data;
-console.log(rawData)
+//create cleanData array
+var cleanData = [];
+
+//get amount of data strings
+var amountDataStrings = rawData.feed.entry.length
+
+//loop through data
+for (var i = 0; i < amountDataStrings; i++) {
+    var AllTrashData = rawData.feed.entry[i].content.$t
+    var SplicedTrashData = AllTrashData.split(',');
+    var street = SplicedTrashData[3].split(':')[1];
+    var houseNumber = SplicedTrashData[4].split(':')[1];
+    //    var fulness = array[11].split(':')[1];
+    //   console.log(fulness)
+
+    //create string
+    var cleanDataString = {
+        "street": street,
+        "houseNumber": houseNumber
+    }
+
+}
+console.log(cleanData)
+
+
 
 // Listen to incoming HTTP requests, can only be used on the server
 WebApp.connectHandlers.use(function (req, res, next) {
