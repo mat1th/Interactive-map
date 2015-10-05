812529e68c42259b515ae966995ea7ad2d1026db
Matth96 <Matthias@192.168.2.43>
2015-10-05 18:52:58 +0200

    Get JSON data on Server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 // marker collection
 var Markers = new Meteor.Collection('markers');
 Meteor.publish("markers", function () {
-  return Markers.find();
+    return Markers.find();
 });
 
+
+//data collection
+var data = new Meteor.Collection('data');
+Meteor.publish("data", function () {
+    return data.find();
+});
+
+
+rawData = HTTP.get("https://spreadsheets.google.com/feeds/list/1mkgRVyHAmTS6VhL8q74XKdsF_adAVdNvTTilJshO5pY/od6/public/values?alt=json").data;
+console.log(rawData)
+
+//POI.push({
+//    layer: 'Openstreetmap',
+//    lat: p.location.latitude,
+//    lng: p.location.longitude,
+//    message: "<h1>" + p.title + "</h1>" + "<md-button ng-controller=\"DialogCtrl\" ng-click=\"showAdvanced('" + p.id + "')\">" + "See more</md-button>",
+//    icon: local_icons.christiansabused_icon
+//})
+
+
 // Listen to incoming HTTP requests, can only be used on the server
-WebApp.connectHandlers.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  return next();
+WebApp.connectHandlers.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return next();
 });
