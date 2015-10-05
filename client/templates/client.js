7a5bdce140fa3c64ad69d8a381078521faaed4fc
Matth96 <Matthias@192.168.2.43>
2015-10-05 18:52:42 +0200

    add compass on map and get location of device

--- client/templates/client.js
+++ client/templates/client.js
@@ -1,49 +1,72 @@
 // on startup run resizing event
-Meteor.startup(function() {
-  $(window).resize(function() {
-    $('#map').css('height', window.innerHeight - 82 - 45);
-  });
-  $(window).resize(); // trigger resize event
+Meteor.startup(function () {
+    $(window).resize(function () {
+        $('#map').css('height', window.innerHeight - 82 - 45);
+    });
+    $(window).resize(); // trigger resize event
 });
 
 // create marker collection
 var Markers = new Meteor.Collection('markers');
-
 Meteor.subscribe('markers');
 
-Template.map.rendered = function() {
-  L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
+//create data collection
+var data = new Meteor.Collection('data');
+Meteor.subscribe('data');
 
-  var map = L.map('map', {
-    doubleClickZoom: false
-  }).setView([49.25044, -123.137], 13);
+//if template "map" is renderd
+Template.map.rendered = function () {
+    //url to images for map
+    L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
 
-  L.tileLayer.provider('Thunderforest.Outdoors').addTo(map);
+    //create leaflet map and start coordiates
+    var map = L.map('map', {
+        doubleClickZoom: false
+    }).setView([52.3547, 4.904], 13);
 
-  map.on('dblclick', function(event) {
-    Markers.insert({latlng: event.latlng});
-  });
+    //map sort
+    L.tileLayer.provider('Thunderforest.Transport').addTo(map);
 
-  var query = Markers.find();
-  query.observe({
-    added: function (document) {
-      var marker = L.marker(document.latlng).addTo(map)
-        .on('click', function(event) {
-          map.removeLayer(marker);
-          Markers.remove({_id: document._id});
+    //if dubble click create a marker
+    map.on('dblclick', function (event) {
+        Markers.insert({
+            latlng: event.latlng
         });
-    },
-    removed: function (oldDocument) {
-      layers = map._layers;
-      var key, val;
-      for (key in layers) {
-        val = layers[key];
-        if (val._latlng) {
-          if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
-            map.removeLayer(val);
-          }
+    });
+
+    //add compass
+    map.addControl(new L.Control.Compass());
+    //add current location icon
+
+    //create style of a marker
+    var markerStyle = {radius: 25, weight:4, borderColor:"rgba(0, 170, 255, 0.7)", color: '#00AAFF', fill: true};
+
+    //add marker to map
+    map.addControl( new L.Control.Gps({style: markerStyle }) );
+
+
+    var query = Markers.find();
+    query.observe({
+        added: function (document) {
+            var marker = L.marker(document.latlng).addTo(map)
+                .on('click', function (event) {
+                    map.removeLayer(marker);
+                    Markers.remove({
+                        _id: document._id
+                    });
+                });
+        },
+        removed: function (oldDocument) {
+            layers = map._layers;
+            var key, val;
+            for (key in layers) {
+                val = layers[key];
+                if (val._latlng) {
+                    if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
+                        map.removeLayer(val);
+                    }
+                }
+            }
         }
-      }
-    }
-  });
+    });
 };
