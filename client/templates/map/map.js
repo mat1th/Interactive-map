72324da3a0c656008526706d64bfaf1b45dde917
Matth96 <Matthias.Dolstra@hva.nl>
2015-10-12 12:18:40 +0200

    fix map

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -27,56 +27,10 @@ Template.map.rendered = function () {
   }
 );
 
-    //heatmap
-    var testData = {
-        max: 8,
-        data: [{
-            lat: 24.6408,
-            lng: 46.7728,
-            count: 3
-        }, {
-            lat: 50.75,
-            lng: -1.55,
-            count: 1
-        }]
-    };
-
-    var cfg = {
-        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-        // if scaleRadius is false it will be the constant radius used in pixels
-        "radius": 2,
-        "maxOpacity": .8,
-        // scales the radius based on map zoom
-        "scaleRadius": true,
-        // if set to false the heatmap uses the global maximum for colorization
-        // if activated: uses the data maximum within the current map boundaries
-        //   (there will always be a red spot with useLocalExtremas true)
-        "useLocalExtrema": true,
-        // which field name in your data represents the latitude - default "lat"
-        latField: 'lat',
-        // which field name in your data represents the longitude - default "lng"
-        lngField: 'lng',
-        // which field name in your data represents the data value - default "value"
-        valueField: 'count'
-    };
-
-    var heatmapLayer = new HeatmapOverlay(cfg);
-
- var map = new L.Map('map-canvas', {
-  center: new L.LatLng(25.6586, -80.3568),
-  zoom: 4,
-  layers: [baseLayer, heatmapLayer]
-});
-
-    heatmapLayer.setData(testData);
-
-
-
 
     //create leaflet map and start coordiates
-//    var map = L.map('map', {
-//        layers: [heatmapLayer],
-//    }).setView([52.3547, 4.904], 13);
+    var map = L.map('map', {
+    }).setView([52.3547, 4.904], 13);
 
     //map sort
     L.tileLayer.provider('Thunderforest.Transport').addTo(map);
