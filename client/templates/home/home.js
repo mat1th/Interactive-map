65ca2edcf9cee2e7fa051af9ab259f89613ee4de
Matth96 <Matthias.Dolstra@hva.nl>
2015-10-14 15:23:55 +0200

    add animation to home

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,3 +1,8 @@
 Template.home.rendered = function () {
 
+    var homepage = document.querySelectorAll('.homepage');
+    TweenMax.from(homepage, 2, {
+        opacity: 0,
+        y: 100
+    });
 }
