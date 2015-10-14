e7c697126ed458a55256723a1a42b86f770efa57
Matth96 <Matthias.Dolstra@hva.nl>
2015-10-14 11:17:45 +0200

    clean up code, and create div marker instead of img marker

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -10,82 +10,46 @@ Meteor.startup(function () {
 var Markers = new Meteor.Collection('markers');
 Meteor.subscribe("markers");
 
+//create trashesCollection
+var trashesCollection = new Meteor.Collection('trashesCollection');
+
 //if template "map" is renderd
 Template.map.rendered = function () {
-
-    //create data collection
-    var trashesCollection = new Meteor.Collection('trashesCollection');
+    //subscribe to trashesCollection
     Meteor.subscribe('trashesCollection', function () {
         var trashesData = trashesCollection.find().fetch();
+        //callback to setTrashes
         setTrashes(trashesData)
     });
-
-    var setTrashes = function (trashesData) {
-        var amountTrashes = trashesData.length,
-            i = 0;
-        console.log(trashesData)
-        for (i; i < amountTrashes; i++) {
-            var longitude = trashesData[i].log;
-            var latitude = trashesData[i].lat;
-            var street = trashesData[i].street;
-
-            L.marker([latitude, longitude], {
-                icon: full
-            }).addTo(map).bindPopup(street);
-        }
-    };
 
     //url to images for map
     var baseLayer = L.tileLayer(
         'http://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png', {
-            attribution: 'Informotion ',
-            maxZoom: 16,
-            minZoom: 13
+            attribution: 'Informotion',
+            maxZoom: 14,
+            minZoom: 14
         });
-
-    //map sort
     //create leaflet map and start coordiates
     var map = L.map('map', {
         center: [52.376956, 4.902756],
-        zoom: 14,
-        opacity: 0,
+        maxZoom: 15,
+        minZoom: 15,
+        zoom: 15,
         layers: [baseLayer]
     });
     L.tileLayer.provider('Hydda.Base').addTo(map);
+    //add bounds to map
+    map.fitBounds([
+     [52.359457601988254, 4.864282608032227],
+    [52.380419542018174, 4.941530227661133]]);
 
-    var trashIcon = L.Icon.extend({
-        options: {
-            iconSize: [20, 50],
-            shadowSize: [50, 64],
-            iconAnchor: [22, 94],
-            shadowAnchor: [4, 62],
-            popupAnchor: [-3, -76]
-        }
+    //create div icon with class trash
+    var trashIcon = L.divIcon({
+        className: 'trash'
     });
-
-    var full = new trashIcon({
-            iconUrl: 'icons/trash.svg'
-        }),
-        halfFull = new trashIcon({
-            iconUrl: 'icons/trash.svg'
-        }),
-        empty = new trashIcon({
-            iconUrl: 'icons/trash.svg'
-        });
-
-    //    var trash = L.layerGroup([twee, drie]);
-    //    var trashFull = L.layerGroup([een, drie]);
 
     //add compass
     map.addControl(new L.Control.Compass());
-    //
-    //    var overlayMaps = {
-    //        "trash": trash,
-    //        "trashFull": trashFull
-    //    };
-
-    //    L.control.layers(overlayMaps).addTo(map);
-
 
     //add current location icon
     //create style of a marker
@@ -97,32 +61,36 @@ Template.map.rendered = function () {
         fill: true
     };
 
-    //add marker to map
+    //add current marker to map
     map.addControl(new L.Control.Gps({
         style: markerStyle
     }));
-    //    var query = Markers.find();
-    //    query.observe({
-    //        added: function (document) {
-    //            var marker = L.marker(document.latlng).addTo(map)
-    //                .on('click', function (event) {
-    //                    map.removeLayer(marker);
-    //                    Markers.remove({
-    //                        _id: document._id
-    //                    });
-    //                });
-    //        },
-    //        removed: function (oldDocument) {
-    //            layers = map._layers;
-    //            var key, val;
-    //            for (key in layers) {
-    //                val = layers[key];
-    //                if (val._latlng) {
-    //                    if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
-    //                        map.removeLayer(val);
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    });
+
+    //set trashes on map
+    var setTrashes = function (trashesData) {
+        var amountTrashes = trashesData.length,
+            i = 0;
+        for (i; i < amountTrashes; i++) {
+            var longitude = trashesData[i].log;
+            var latitude = trashesData[i].lat;
+            var street = trashesData[i].street;
+            var number = trashesData[i].houseNumber;
+            L.marker([latitude, longitude], {
+                icon: trashIcon,
+                riseOnHover: true,
+            }).addTo(map).bindPopup(street + number);
+        }
+    };
+
+    //create layers
+    //    var trash = L.layerGroup([twee, drie]);
+    //    var trashFull = L.layerGroup([een, drie]);
+
+    //    var overlayMaps = {
+    //        "trash": trash,
+    //        "trashFull": trashFull
+    //    };
+
+    //    L.control.layers(overlayMaps).addTo(map);
+
 };
