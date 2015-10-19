cdfaaaa9adeeda6425b2139b13919c79c4760afa
Matth96 <Matthias.Dolstra@hva.nl>
2015-10-19 16:10:06 +0200

    add header and filter to html

--- server/policy.js
+++ server/policy.js
@@ -3,3 +3,4 @@ BrowserPolicy.content.allowOriginForAll("meteor.local");
 BrowserPolicy.content.allowOriginForAll("*.openstreetmap.org");
 BrowserPolicy.content.allowOriginForAll("*.tile.thunderforest.com");
 BrowserPolicy.content.allowOriginForAll("*.basemaps.cartocdn.com");
+BrowserPolicy.content.allowOriginForAll("fonts.gstatic.com");
