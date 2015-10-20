7b99bdb4a5018dfa4b04ae6c5daf79de4b35571d
Matth96 <Matthias.Dolstra@hva.nl>
2015-10-20 10:49:02 +0200

    add css next

--- mobile-config.js
+++ mobile-config.js
@@ -1,9 +1,16 @@
 // basic info
 App.info({
-  name: 'Interactive map',
-  description: 'An interactive map with geo data',
-  author: 'Matthias Dolstra',
-  website: 'http://mdolstra.me'
+    name: 'Interactive map',
+    description: 'An interactive map with geo data',
+    author: 'Matthias Dolstra',
+    website: 'http://mdolstra.me'
+});
+
+Plugin.registerCompiler({
+    filenames: ['xxx.next.css'],
+    archMatching: 'web',
+}, function () {
+    return new CssnextCompiler(cssnextOptions)
 });
 
 // CORS for Meteor app
