48d39073a7b74eade12b63b0a64ee58528c58ccf
Matth96 <Matthias.Dolstra@hva.nl>
2015-10-20 11:55:37 +0200

    delete home page

--- client/templates/home/home.js
+++
@@ -1,8 +0,0 @@
-Template.home.rendered = function () {
-
-    var homepage = document.querySelectorAll('.homepage');
-    TweenMax.from(homepage, 2, {
-        opacity: 0,
-        y: 100
-    });
-}
